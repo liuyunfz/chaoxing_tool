e1b1d762a37eae3318a1ee647b63f6f61212369c
liuyunfz <957843164@qq.com>
2023-02-12 01:12:05 +0800

    feat: function loads from dir

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import yaml
 import os
 from loguru import logger
 import sys
+import importlib
 from classis.User import User
 from classis.UserLogger import UserLogger
 from utils import *
@@ -42,7 +43,41 @@ if __name__ == '__main__':
             except Exception as e:
                 log.error(e)
     else:
-        pass    # TODO 完善cookie登录与判断
+        pass  # TODO 完善cookie登录与判断
     log.info("运行读取课程任务")
     user.getCourse()
     log.success("课程读取成功")
+    if not user.course_list:
+        log.info("读取到您没有可执行的课程，如果与您的实际情况不符请提交issue")
+        exit()
+
+    functions = []
+    path_list = os.listdir('./functions')
+    for module_path in path_list:
+        logger.debug("Loading function %s" % module_path)
+        functions.append(importlib.import_module("functions.%s" % module_path))
+        logger.success("Loaded %s successfully" % module_path)
+
+    menu_str = "菜单"
+    for fun_ind in range(len(functions)):
+        menu_str += f"\n{fun_ind + 1}.{functions[fun_ind].__disName__}"
+
+    while True:
+        try:
+            print(menu_str)
+            fun_i = int(input("\n请输入您要使用功能的序号:"))
+            func = functions[fun_i-1]
+            os.system("cls")
+            print("\n".join([f"功能名称: {func.__disName__}",
+                             f"作者: {func.__author__}",
+                             f"使用须知: {func.__description__}"])
+                  )
+            if input("\n回车确认使用本功能，其他输入则会退回主菜单:") == "":
+                func.run(user, log)
+            else:
+                os.system("cls")
+                continue
+
+        except Exception as e:
+            log.error("功能调用失败，请检查输入的功能序号")
+            logger.error(e)
