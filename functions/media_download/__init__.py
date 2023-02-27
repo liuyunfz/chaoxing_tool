02b9244431140468cdf030e36092fcd19f78777d
liuyunfz <957843164@qq.com>
2023-02-27 15:53:20 +0800

    feat: download media function

---
+++ functions/media_download/__init__.py
@@ -0,0 +1,89 @@
+# _*_ coding:utf-8 _*_
+# author: liuyunfz
+
+__name__ = "media_download"
+__author__ = "liuyunfz"
+__disName__ = " 📂 下载课程中的资源"
+__description__ = """下载课程章节中的媒体资源
+包括视频、Word、PPT、音频等文件
+"""
+
+import os
+import re
+import time
+
+import loguru
+import classis.User
+from functions.media_download.deal_media import MediaDownload
+
+
+def run(user: classis.User.User, log):
+    os.system("cls")
+    for i in range(len(user.course_list)):
+        print("%d.%s" % (i + 1, user.course_list[i].course_name))
+
+    course_choice = []
+    while not course_choice:
+        choice = input("请选择您下载媒体的课程序号，或者输入q退出本功能\n序号:")
+        if choice == "q":
+            return
+        else:
+            try:
+                course_choice.append(user.course_list[int(choice) - 1])
+            except Exception as e:
+                log.error("序号输入错误，请尝试重新输入")
+                loguru.logger.error(e)
+                course_choice.clear()
+                pass
+
+    chapter_media = MediaDownload(user, course_choice[0], log)
+    chapter_media.deal_course()
+    chapter_choice = []
+    while not chapter_choice:
+        choice = input("请选择将下载资源所在的课程序号，并用逗号分割。\n如果需要读取所有资源则直接回车即可。或者输入q退出本功能\n序号:")
+        if not choice:
+            chapter_choice = chapter_media.chapter_all
+        elif choice == "q":
+            return
+        else:
+            try:
+                for i in re.split("[,，]", choice):
+                    chapter_choice.append(chapter_media.chapter_all[int(i) - 1])
+            except Exception as e:
+                log.error("序号输入错误，请尝试重新输入")
+                loguru.logger.error(e)
+                chapter_choice.clear()
+                pass
+
+    media_all = []
+    for chapter_item in chapter_choice:
+        log.info(f"开始读取章节'{chapter_item.get('name')}'")
+        chapter_medias = chapter_media.deal_chapter(chapter_item)
+        count = 0
+        for _ in chapter_medias:
+            _att = _.get("attachments")
+            count += len(_att)
+            media_all += _att
+        log.info(f"章节'{chapter_item.get('name')}'读取完成，共{count}个资源")
+
+    log.success(f"读取完毕，共计{len(media_all)}个资源，即将展示..")
+    time.sleep(0.4)
+    for media in media_all:
+        print(media_all.index(media), ".", media.get("property").get("name") if media.get("property").get("name") else media.get("property").get("bookname"))
+    log.success("资源展示完成")
+    while True:
+        choice = input("请选择您要下载媒体的序号，用逗号分割，或者输入q退出本功能\n序号:")
+        if choice == "q":
+            return
+        else:
+            try:
+                for i in re.split("[,，]", choice):
+                    chapter_media.do_download("./downloads", attachment=media_all[int(i)])
+                break
+            except Exception as e:
+                log.error("序号输入错误，请尝试重新输入")
+                loguru.logger.error(e)
+                chapter_choice.clear()
+                pass
+    log.success("下载功能执行完毕，回车返回主菜单")
+    input()
---
+++ functions/media_download/deal_media.py
@@ -0,0 +1,86 @@
+# _*_ coding:utf-8 _*_
+# author: liuyunfz
+import json
+import os.path
+import time
+
+import requests
+
+import classis.User
+from functions.deal_mission import DealCourse
+from utils import doGet
+
+
+class MediaDownload(DealCourse):
+    def __init__(self, user: classis.User.User, course: classis.User.Course, log):
+        super().__init__(user, course, log)
+        self.chapter_all = []
+
+    def deal_course(self):
+        self.log.info(f"获取'{self.course_name}'课程的章节中...")
+        self.course.get_chapter(self.user.headers) if not self.course.chapter_list else None
+        if self.course.chapter_list:
+            self.log.success(f"获取'{self.course_name}'课程章节成功，即将展示")
+            time.sleep(0.4)
+            self.chapter_all = []
+            ind = 1
+            for catalog_item in self.course.chapter_list:
+                print(catalog_item.get("catalog_name"))
+                for chapter_item in catalog_item.get("child_chapter"):
+                    print(str(ind) + ". ", "----" * (chapter_item.get("depth") + 1), chapter_item.get("name"))
+                    self.chapter_all.append(chapter_item)
+                    ind += 1
+                print("🔷" * 35)
+            self.log.success(f"'{self.course_name}'课程章节展示完毕")
+        else:
+            self.log.warning(f"'{self.course_name}'课程章节数为零，请核实或检查网络问题。如有出入请反馈issue")
+
+    def deal_chapter(self, chapter_item: dict):
+        return super().deal_chapter(chapter_item)
+
+    def read_card_count(self, knowledge_id) -> int:
+        return super().read_card_count(knowledge_id)
+
+    def do_download(self, path="", attachment: dict = {}):
+        objectid = attachment.get("property").get("objectid")
+        name = attachment.get("property").get("name") if attachment.get("property").get("name") else attachment.get("property").get("bookname")
+        if objectid is None:
+            self.log.error(name + "文件ID获取失败，将跳过该文件")
+            return
+        _headers = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'Host': 'mooc1-2.chaoxing.com',
+            'Referer': 'https://mooc1-2.chaoxing.com/ananas/modules/audio/index.html?v=2022-1028-1705',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+        download_headers = {
+            "referer": "https://mooc1-2.chaoxing.com/ananas/modules/video/index.html?v=2021-0924-1446",
+            "Host": "s1.ananas.chaoxing.com"
+        }
+        download_headers.update(self.user.headers)
+        _headers.update(self.user.headers)
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        _status = doGet(url="https://mooc1-2.chaoxing.com/ananas/status/{}?_dc={}".format(objectid, int(time.time() * 1000)), headers=_headers)
+        _status_json = json.loads(_status)
+        filename = _status_json.get('filename')
+        if _status_json.get("httphd"):
+            _url = _status_json.get('httphd')
+        elif _status_json.get("pagenum") is not None:
+            _url = _status_json.get('pdf')
+        else:
+            _url = _status_json.get('http')
+        with open(path + "/" + name.split('.')[0] + '.' + filename.split('.')[-1], "wb") as f:
+            rsp = requests.get(url=_url, headers=download_headers, stream=True)
+            length_already = 0
+            length_all = int(rsp.headers['content-length'])
+            self.log.info(f"开始下载文件'{name}'")
+            for chunk in rsp.iter_content(chunk_size=5242880):
+                if chunk:
+                    length_already += len(chunk)
+                    print("\r下载进度：%d%%" % int(length_already / length_all * 100), end="", flush=True)
+                    f.write(chunk)
+            self.log.success(f"'{name}'下载完成")
